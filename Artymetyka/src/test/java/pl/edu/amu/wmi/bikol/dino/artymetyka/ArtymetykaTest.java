7e608cd9a7fe23cbc27e7b60660dde09b5d14c44
s350714 <s350714@pcd2o.labs.wmi.amu.edu.pl>
2021-01-21 15:41:00 +0100

    Utworzenie nowego testu

--- Artymetyka/src/test/java/pl/edu/amu/wmi/bikol/dino/artymetyka/ArtymetykaTest.java
+++ Artymetyka/src/test/java/pl/edu/amu/wmi/bikol/dino/artymetyka/ArtymetykaTest.java
@@ -56,4 +56,14 @@ public class ArtymetykaTest {
         String result = Artymetyka.mnozenie(a, b);
         assertEquals(expResult, result);
     }
+    
+    @Test
+    public void testMnozenie5() {
+        System.out.println("mnozenie5");
+        String a = "1.11";
+        String b = "-1.11";
+        String expResult = "-1.2321";
+        String result = Artymetyka.mnozenie(a, b);
+        assertEquals(expResult, result);
+    }
 }
