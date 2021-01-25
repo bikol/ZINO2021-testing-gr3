2ac0c9a78690e5fbdfaab211aab85d2ae1ec59cb
s458026 <s458026@pcd2e.labs.wmi.amu.edu.pl>
2021-01-25 20:33:00 +0100

    Naprawa implementacji

--- Artymetyka/src/main/java/pl/edu/amu/wmi/bikol/dino/artymetyka/Artymetyka.java
+++ Artymetyka/src/main/java/pl/edu/amu/wmi/bikol/dino/artymetyka/Artymetyka.java
@@ -12,6 +12,26 @@ package pl.edu.amu.wmi.bikol.dino.artymetyka;
 public class Artymetyka {
     
     public static String mnozenie(String a, String b){
+        if(a.indexOf('/')!=-1 || (b.indexOf('/')!=-1))
+        {
+            char a1 = a.charAt(0);
+            char a2 = a.charAt(2);
+            char b1 = b.charAt(0);
+            char b2 = b.charAt(2);
+            
+            double x1 = (double) Character.digit(a1, 10);
+            double y1 = (double) Character.digit(a2, 10);
+            
+            double x2 = (double) Character.digit(b1, 10);
+            double y2 = (double) Character.digit(b2, 10);
+            
+            double x = x1/y1;
+            double y = x2/y2;
+         
+            return String.format("%s", (double)x*y);
+            
+        }
+        else{
         double double_a = Double.parseDouble(a.trim());
         double double_b = Double.parseDouble(b.trim());
         if(double_a * double_b == (long) (double_a * double_b))
