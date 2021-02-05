a86cd54b6472571a6ab073bb77a58b2a71a8b618
wawrzon007 <222michal100@wp.pl>
2021-02-05 11:21:01 +0100

    Update Artymetyka.java

--- Artymetyka/src/main/java/pl/edu/amu/wmi/bikol/dino/artymetyka/Artymetyka.java
+++ Artymetyka/src/main/java/pl/edu/amu/wmi/bikol/dino/artymetyka/Artymetyka.java
@@ -12,7 +12,7 @@ package pl.edu.amu.wmi.bikol.dino.artymetyka;
 public class Artymetyka {
     
     public static String mnozenie(String a, String b){
-        if(a.indexOf('/')!=-1 || (b.indexOf('/')!=-1))
+        if(a.indexOf('/')!=-1 && (b.indexOf('/')!=-1))
         {
             char a1 = a.charAt(0);
             char a2 = a.charAt(2);
@@ -24,13 +24,28 @@ public class Artymetyka {
             
             double x2 = (double) Character.digit(b1, 10);
             double y2 = (double) Character.digit(b2, 10);
-            
             double x = x1/y1;
             double y = x2/y2;
-         
             return String.format("%s", (double)x*y);
             
         }
+<<<<<<< Updated upstream
+=======
+        else if(b.indexOf('/')!=-1)
+        {
+
+            char b1 = b.charAt(0);
+            char b2 = b.charAt(2);
+            
+            double x=Integer.parseInt(a);
+            
+            double x2 = (double) Character.digit(b1, 10);
+            double y2 = (double) Character.digit(b2, 10);
+            
+            double y = x2/y2;
+            return String.format("%s", (double)x*y);       
+        }
+>>>>>>> Stashed changes
         else{
         double double_a = Double.parseDouble(a.trim());
         double double_b = Double.parseDouble(b.trim());
