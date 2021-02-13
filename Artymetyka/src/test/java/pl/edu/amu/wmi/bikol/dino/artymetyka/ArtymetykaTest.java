3b4cf32afd3e8ce87f7ab98218787170205fd2a2
s458015 <s458015@pcd2n.labs.wmi.amu.edu.pl>
2021-02-13 20:43:21 +0100

    Test nie przechodzi

--- Artymetyka/src/test/java/pl/edu/amu/wmi/bikol/dino/artymetyka/ArtymetykaTest.java
+++ Artymetyka/src/test/java/pl/edu/amu/wmi/bikol/dino/artymetyka/ArtymetykaTest.java
@@ -47,13 +47,12 @@ public class ArtymetykaTest {
         assertEquals(expResult, result);
     }
 
-     @Test
+    @Test
     public void testMnozenie4() {
-        System.out.println("mnozenie3");
+        System.out.println("mnozenie4");
         String a = "1.1";
         String b = "1.1";
         String expResult = "1.21";
-
         String result = Artymetyka.mnozenie(a, b);
         assertEquals(expResult, result);
     }
@@ -61,11 +60,41 @@ public class ArtymetykaTest {
     @Test
     public void testMnozenie5() {
         System.out.println("mnozenie5");
-        String a = "0.1";
-        String b = "0.1";
-        String expResult = "0.01";
-
+        String a = "1.11";
+        String b = "-1.11";
+        String expResult = "-1.2321";
         String result = Artymetyka.mnozenie(a, b);
         assertEquals(expResult, result);
     }
+    
+    @Test
+    public void testMnozenie6() {
+        System.out.println("mnozenie6");
+        String a = "1.001";
+        String b = "-1.001";
+        String expResult = "-1.002001";
+        String result = Artymetyka.mnozenie(a, b);
+        assertEquals(expResult, result);
+    }
+    
+    @Test
+    public void testMnozenie7() {
+        System.out.println("mnozenie7");
+        String a = "1/2";
+        String b = "1/2";
+        String expResult = "0.25";
+        String result = Artymetyka.mnozenie(a, b);
+        assertEquals(expResult, result);
+    }
+    
+     @Test
+    public void testMnozenie8() {
+        System.out.println("mnozenie8");
+        String a = "3/1";
+        String b = "1";
+        String expResult = "3";
+        String result = Artymetyka.mnozenie(a, b);
+        assertEquals(expResult, result);
+    }
+    
 }
